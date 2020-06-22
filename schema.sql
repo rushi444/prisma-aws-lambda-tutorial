ec79ccc1bcae4c7ca25f2dbf0e53da0a6dbd8b4e
rushi444 <rushi44@gmail.com>
2020-06-22 09:22:09 -0700

    up and running on aws

--- schema.sql
+++ schema.sql
@@ -1,10 +1,12 @@
-CREATE TABLE "public"."User" (
+CREATE TABLE "public"."User"
+(
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255),
   email VARCHAR(255) UNIQUE NOT NULL
-);
+)
 
-CREATE TABLE "public"."Post" (
+CREATE TABLE "public"."Post"
+(
   id SERIAL PRIMARY KEY NOT NULL,
   title VARCHAR(255) NOT NULL,
   "createdAt" TIMESTAMP NOT NULL DEFAULT now(),
@@ -12,11 +14,12 @@ CREATE TABLE "public"."Post" (
   published BOOLEAN NOT NULL DEFAULT false,
   "authorId" INTEGER NOT NULL,
   FOREIGN KEY ("authorId") REFERENCES "public"."User"(id)
-);
+)
 
-CREATE TABLE "public"."Profile" (
+CREATE TABLE "public"."Profile"
+(
   id SERIAL PRIMARY KEY NOT NULL,
   bio TEXT,
   "userId" INTEGER UNIQUE NOT NULL,
   FOREIGN KEY ("userId") REFERENCES "public"."User"(id)
-);
+)
